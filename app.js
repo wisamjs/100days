4e128aa62cb4d2dcc048704d08fcb1c64d27df05
wzaghal <wisamzaghal@gmail.com>
2015-04-03 14:09:44 -0400

    add initial functionality

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
-angular.module('100days',[])
+'use strict';
 
-.controller('MainController',function(){
+angular.module('100days',[])
+.factory('people',function(){
+  var service = {};
+
+  var list = [{
+    name:'Winter',
+    image: 'http://placehold.it/250/227594/fff',
+    description:'Vestibulum id ligula porta felis euismod semper.',
+    link:'http://twitter.com',
+    keywords: ' Magna Pharetra Tristique Venenatis Fringilla'
+    },{
+      name: 'Wisam',
+      image: 'http://placehold.it/250/944122/fff',
+      description: 'Cras justo odio, dapibus ac facilisis in, egestas eget quam.',
+      link:'http://google.com',
+      keywords: 'Elit Cras Cursus Fringilla Bibendum'
+    }];
+
+  service.getAll = function(){
+    return list;
+  }
+  return service;
+})
+.controller('MainController',function(people){
   var vm = this;
-  vm.test = 'hello!';
+
+  //get all people from service
+  vm.list = people.getAll();
 });
